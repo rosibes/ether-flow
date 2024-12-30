8cbb8fd44687fba0290a03a292fd71969fe8aa24
rosibes <seby.boiciuc03@e-uvt.ro>
2024-12-30 19:30:53 +0200

    fixed the footer glitch

--- client/src/components/ConnectWalletButton.jsx
+++ client/src/components/ConnectWalletButton.jsx
@@ -17,12 +17,7 @@ function ConnectWalletButton({ setAccount, setProvider }) {
    <button className="bg-blue-600 text-white px-6 py-3 rounded-xl font-semibold text-lg hover:bg-blue-900 transition duration-300"
 
    onClick={connectWallet}>Connect Web3 Wallet</button>;
-    <footer className=" text-gray-100 w-full py-4 mt-4 text-sm border-t absolute bottom-0">
-          Developed by <a
-            href="https://x.com/rosibes27"
-            target="_blank"
-            rel="noopener noreferrer" className="text-gray-100 text-lg">rosibes</a>
-        </footer>
+   
    </div>
 }
 
