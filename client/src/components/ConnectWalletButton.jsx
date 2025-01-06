ae62bec11d6002d1f0204c6de27ed0cad848b78d
rosibes <seby.boiciuc03@e-uvt.ro>
2025-01-06 19:22:04 +0200

    removed the ;

--- client/src/components/ConnectWalletButton.jsx
+++ client/src/components/ConnectWalletButton.jsx
@@ -16,7 +16,7 @@ function ConnectWalletButton({ setAccount, setProvider }) {
   return <div className="bg-custom-blue h-screen flex justify-center items-center">
    <button className="bg-blue-600 text-white px-6 py-3 rounded-xl font-semibold text-lg hover:bg-blue-900 transition duration-300"
 
-   onClick={connectWallet}>Connect Web3 Wallet</button>;
+   onClick={connectWallet}>Connect Web3 Wallet</button>
    
    </div>
 }
